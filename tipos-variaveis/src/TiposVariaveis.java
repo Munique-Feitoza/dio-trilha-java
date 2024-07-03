fc8203244782132766a2d018be4a371dd1271ec5
Munique-Feitoza <muniquefeitoz4@gmail.com>
2024-07-03 09:35:18 -0300

    tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+
+        //casting
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        //variaveis VS constantes
+        int numero = 1;
+        numero = 2;
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(numero);
+        System.out.println(VALOR_DE_PI);
+
     }
 }
