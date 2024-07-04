c7a2da96e8a273706a819c5a9af7b7fdaec0bf08
Munique-Feitoza <muniquefeitoz4@gmail.com>
2024-07-04 08:10:56 -0300

    operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,41 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Linguagem" + " Java";
+        System.out.println(nomeCompleto);
+
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+
+        //Ternarios
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+        resultado = "verdadeiro";
+        else
+        resultado = "falso";
+        */
+
+        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        String resultado = (a==b) ? "true" : "false";
+
+        System.out.println(resultado);
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -5,14 +5,18 @@ public class TiposVariaveis {
 
         //casting
         short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
+        int numeroNormal = numeroCurto + 1;
         short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto);
+        System.out.println(numeroNormal);
+        System.out.println(numeroCurto2);
 
         //variaveis VS constantes
         int numero = 1;
         numero = 2;
+        int numeros = numero + numeroCurto;
         final double VALOR_DE_PI = 3.14;
-        System.out.println(numero);
+        System.out.println(numeros);
         System.out.println(VALOR_DE_PI);
 
     }
