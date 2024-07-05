fab52e49e4fd1d07e79c7513656d5b8651f5b3ad
Munique-Feitoza <muniquefeitoz4@gmail.com>
2024-07-05 03:30:31 -0300

    controle de fluxo

---
+++ estruturas-de-repeticao/src/ExemploFor.java
@@ -0,0 +1,22 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+    
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+    
+    }
+}
+
+/*
+public class ExemploFor {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+    }
+}
+ */
