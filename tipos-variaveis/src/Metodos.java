7ce71da688c34665f34b2cc433fc2ec0a92b7732
Munique-Feitoza <muniquefeitoz4@gmail.com>
2024-07-05 01:14:39 -0300

    metodos

--- tipos-variaveis/src/Metodos.java
+++ tipos-variaveis/src/Metodos.java
@@ -2,7 +2,7 @@ public class Metodos {
     public static void main(String[] args) {
             public double somar(int num1, int num2){
                 //LOGICA - FINALIDADE DO MÉTODO
-                return num1 + num2 ;
+            return num1 + num2 ;
             }
             
             public void imprimir(String texto){
